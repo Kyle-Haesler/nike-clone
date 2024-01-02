4f7489c2644c90a34dee6297c1b62cabf90e4fa4
Kyle Haesler <kyle.haesler@gmail.com>
2024-01-02 13:58:40 -0500

    working on nav page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { CustomerReviews, Footer, SuperQuality, Services, Subscribe, SpecialOffer, PopularProducts, Hero } from "./sections"
+import Nav from "./components/Nav"
 
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide: padding-r padding-b">
       <Hero />
     </section>
